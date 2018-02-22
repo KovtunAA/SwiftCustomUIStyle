c9c3e27917f582d1cfcf0cef96edbf1d0b0e63ff
KovtunAA <marta.sladkih@gmail.com>
2018-02-22 10:54:50 +0200

    Initial Commit

---
+++ SwiftCustomUIStyle/SwiftCustomUIStyle.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCustomUIStyle.h
+//  SwiftCustomUIStyle
+//
+//  Created by Mac on 2/22/18.
+//  Copyright © 2018 attractgroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftCustomUIStyle.
+FOUNDATION_EXPORT double SwiftCustomUIStyleVersionNumber;
+
+//! Project version string for SwiftCustomUIStyle.
+FOUNDATION_EXPORT const unsigned char SwiftCustomUIStyleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCustomUIStyle/PublicHeader.h>
+
+
